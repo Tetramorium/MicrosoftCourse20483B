498dfd989bffd26c1b651861afc86f702e7d1d34
Jelmer Mulder <9375891+Tetramorium@users.noreply.github.com>
2018-02-06 23:09:17 +0100

    Added Module7

---
+++ Module7/Grades.DataModel/customGrade.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Grades.DataModel
+{
+    public partial class Grade
+    {
+        public bool ValidateAssessmentDate(DateTime _Date)
+        {
+            if (_Date > DateTime.Now)
+            {
+                throw new ArgumentOutOfRangeException("Assessment Date", "Assessment date must be on or before the current date");
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public bool ValidateAssessmentGrade(string assessment)
+        {
+            Match matchGrade = Regex.Match(assessment, @"^[A-E][+-]?$");
+            if (!matchGrade.Success)
+            {
+                throw new ArgumentOutOfRangeException("Assessment", "Assessment grade must be in the range A+ to E-");
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
---
+++ Module7/GradesPrototype/Controls/GradeDialog.xaml.cs
@@ -0,0 +1,52 @@
+﻿using GradesPrototype.Services;
+using System;
+using System.Windows;
+using Grades.DataModel;
+
+namespace GradesPrototype.Controls
+{
+    /// <summary>
+    /// Interaction logic for GradeDialog.xaml
+    /// </summary>
+    public partial class GradeDialog : Window
+    {
+        public GradeDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void GradeDialog_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Display the list of available subjects in the subject ListBox
+            foreach (Subject subj in SessionContext.DBContext.Subjects)
+            {
+                subject.Items.Add(subj.Name);
+            }
+ 
+             // Set default values for the assessment date and subject
+            assessmentDate.SelectedDate = DateTime.Now;
+            subject.SelectedValue = subject.Items[0];
+        }
+
+        // If the user clicks OK to save the Grade details, validate the information that the user has provided
+        private void ok_Click(object sender, RoutedEventArgs e)
+        {
+            // Data validation functionality to be added in Exercise 3
+            try
+            {
+                Grades.DataModel.Grade newGrade = new Grades.DataModel.Grade();
+                testGrade.ValidateAssessmentDate(assessmentDate.SelectedDate.Value);
+                testGrade.ValidateAssessmentGrade(assessmentGrade.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error creating assessment", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }            
+
+            // Indicate that the data is valid
+            this.DialogResult = true;
+        }
+
+    }
+}
