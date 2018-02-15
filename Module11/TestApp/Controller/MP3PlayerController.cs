4f849af699e8e81dc67a898afed5659dc52580a0
Jelmer Mulder <9375891+Tetramorium@users.noreply.github.com>
2018-02-15 11:47:39 +0100

    Added Module11

---
+++ Module11/TestApp/Controller/MP3PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Office.Interop.Word;
+
+namespace TestApp.Controller
+{
+    public class MP3PlayerController : IDisposable
+    {
+        public bool Repeat { get; set; }
+        public bool IsPlaying { get; set; }
+
+        private bool _isDisposed;
+
+        public MP3PlayerController(string fileName)
+        {
+            const string FORMAT = @"open ""{0}"" type mpegvideo alias MediaFile";
+            string command = string.Format(FORMAT, fileName);
+            mciSendString(command, null, 0, IntPtr.Zero);
+        }
+
+        public void Play()
+        {
+            string command = "play MediaFile";
+            if (Repeat) command += " REPEAT";
+            mciSendString(command, null, 0, IntPtr.Zero);
+
+            IsPlaying = true;
+        }
+
+        public void Stop()
+        {
+            string command = "stop MediaFile";
+            mciSendString(command, null, 0, IntPtr.Zero);
+
+            IsPlaying = false;
+        }
+
+        [DllImport("winmm.dll")]
+        private static extern long mciSendString(string strCommand,
+       StringBuilder strReturn, int iReturnLength, IntPtr hwndCallback);
+
+        public void Dispose()
+        {
+            if (!this._isDisposed)
+            {
+                string command = "close MediaFile";
+                mciSendString(command, null, 0, IntPtr.Zero);
+                this._isDisposed = true;
+                GC.SuppressFinalize(this);
+            }
+        }
+    }
+}
---
+++ Module11/TestApp/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TestApp.Controller;
+
+namespace TestApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MP3PlayerController musicPlayer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            string file = @"Media/Music/Bretagne.mp3";
+
+            musicPlayer = new MP3PlayerController(file);
+        }
+
+        private void bt_PlaySong_Click(object sender, RoutedEventArgs e)
+        {
+            if (!musicPlayer.IsPlaying)
+            {
+                musicPlayer.Play();
+                bt_PlaySong.Content = "Pause Song";
+            }
+            else
+            {
+                musicPlayer.Stop();
+                bt_PlaySong.Content = "Play Song";
+            }
+        }
+    }
+}
