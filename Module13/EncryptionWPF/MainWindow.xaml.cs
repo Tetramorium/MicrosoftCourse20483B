a1a25afd6641f7c298fa54e719a6c060b55377ae
Jelmer Mulder <9375891+Tetramorium@users.noreply.github.com>
2018-02-20 11:59:58 +0100

    added Symmetric Encryption and Decryption

---
+++ Module13/EncryptionWPF/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using EncryptionWPF.Tools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EncryptionWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Generate salt
+            byte[] salt = Encryption.GetSalt(12);
+            string password = "abc123";
+
+            string sensitiveData = "Mijn wachtwoord is abc123";
+
+            byte[] bytes = Encoding.ASCII.GetBytes(sensitiveData);
+
+            // Initial call to setup to set up keys for this run
+
+            Encryption.Setup(password, salt);
+
+            var test = Encryption.SymmetricEncryption(bytes);
+
+            var test2 = Encryption.SymmetricDecryption(test);
+        }
+    }
+}
---
+++ Module13/EncryptionWPF/Tools/Encryption.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Security.Cryptography;
+using System.IO;
+
+namespace EncryptionWPF.Tools
+{
+    public static class Encryption
+    {
+        private static byte[] rgbKey;
+        private static byte[] rgbIV;
+        private static AesManaged algorithm = new AesManaged();
+
+        public static void Setup(string _Password, byte[] _Salt)
+        {
+            Rfc2898DeriveBytes rgb = new Rfc2898DeriveBytes(_Password, _Salt);
+
+            algorithm.Padding = PaddingMode.PKCS7;
+
+            rgbKey = rgb.GetBytes(algorithm.KeySize / 8);
+            rgbIV = rgb.GetBytes(algorithm.BlockSize / 8);
+        }
+
+        public static byte[] SymmetricEncryption(byte[] _Data)
+        {
+            byte[] encryptedData;
+
+            using (MemoryStream bufferStream = new MemoryStream())
+            {
+                ICryptoTransform algorithmEncryptor = algorithm.CreateEncryptor(rgbKey, rgbIV);
+
+                using (CryptoStream cryptoStream = new CryptoStream(bufferStream, algorithmEncryptor, CryptoStreamMode.Write))
+                {
+                    cryptoStream.Write(_Data, 0, _Data.Length);
+
+                    cryptoStream.FlushFinalBlock();
+
+                    cryptoStream.Close();
+                    bufferStream.Close();
+                }
+
+                encryptedData = bufferStream.ToArray();
+            }
+
+            return encryptedData;
+        }
+
+        public static string SymmetricDecryption(byte[] _EncryptedData)
+        {
+            string data;
+
+            AesManaged algorithm = new AesManaged();
+
+            using (MemoryStream bufferStream = new MemoryStream(_EncryptedData))
+            {
+                ICryptoTransform algorithmDecryptor = algorithm.CreateDecryptor(rgbKey, rgbIV);
+
+                using (CryptoStream cryptoStream = new CryptoStream(bufferStream, algorithmDecryptor, CryptoStreamMode.Read))
+                {
+                    using (StreamReader streamReader = new StreamReader(cryptoStream))
+                    {
+                        data = streamReader.ReadToEnd();
+                    }
+                }
+            }
+
+            return data;
+        }
+
+
+
+        // https://codereview.stackexchange.com/a/93622
+        // Random salt Generator
+
+        public static byte[] GetSalt(int maximumSaltLength)
+        {
+            var salt = new byte[maximumSaltLength];
+            using (var random = new RNGCryptoServiceProvider())
+            {
+                random.GetNonZeroBytes(salt);
+            }
+
+            return salt;
+        }
+    }
+}
